d16f0c6e1cb27507548f79c0a52f1cfcde678d50
O'Leang <O'Leang>
2017-05-06 14:04:20 +0700

    nong

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main{
 	public static void main(){
+		system.out.print("Hello My name is Nong");
 		system.out.print("Hello Java");
 		system.out.print("Hello Annie");
 		system.out.print("Hello My name is Stop");
