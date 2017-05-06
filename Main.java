736837ecade3a0f09d4e5ec247f43c6621c34028
O'Leang <O'Leang>
2017-05-06 14:37:19 +0700

    Hello

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main{
 	public static void main(){
 		system.out.print("Hello My name is Nong");
 		system.out.print("Hello Java");
-		system.out.print("Hellooooo My name is Tom");
+		system.out.print("Hello My name is Tom");
 		for(int i=0; i< 10; i++){
 			
 		}
