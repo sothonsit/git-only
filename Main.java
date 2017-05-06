3aa11a9832a6eb95d5880e198fe1265b2bb08bef
O'Leang <O'Leang>
2017-05-06 11:39:57 +0700

    modifly Hello Tom

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main{
 	public static void main(){
 		system.out.print("Hello My name is Nong");
 		system.out.print("Hello2");
-		system.out.print("Hello3");
+		system.out.print("Hello My name is Tom");
 		system.out.print("Hello4");
 		system.out.print("Hello5");
 		system.out.print("Hello6");
