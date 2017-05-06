d1059fa06e0ba1f86d440cf7b8bfcc86548d2189
O'Leang <O'Leang>
2017-05-06 14:03:59 +0700

    tom

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main{
 	public static void main(){
 		system.out.print("Hello My name is Nong");
 		system.out.print("Hello Java");
+		system.out.print("Hello My name is Tom");
 		system.out.print("Hello Annie");
 		system.out.print("Hello My name is Stop");
 		system.out.print("Hello Proof");
